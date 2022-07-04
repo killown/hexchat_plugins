0c6f7135238c862844f978983222e9d82ff99cf5
killown <systemofdown@gmail.com>
2022-07-04 19:17:57 -0300

    Update filter-messages.py

--- filter-messages.py
+++ filter-messages.py
@@ -1,21 +1,27 @@
 #killown irc
-#note in case you need to send additional message
-#hexchat.command('SAY {}'.format("ok man, this is working!"))
 
 __module_name__ = "filter"
 __module_version__ = "1.0"
 __module_description__ = "Filter messages"
 
 import hexchat
+from pathlib import Path
+import os
 
-filters = ["https://bit.ly", "https://bpa.st/"]
+path = os.path.join(Path.home(), ".config/hexchat/addons/filter-words-list.txt")
+if not os.path.exists(path):
+    os.mknod(path)
 
+filters = [i.strip() for i in open(path, "r").readlines()]
+
+# Called when you send a message
 def replace_url(word, word_eol, userdata):
 	# Skip replacement if string does not match
     if not any(filter for filter in filters if filter in word_eol[0]) and "://" in word_eol[0]:
         url = [i for i in word_eol[0].split() if "://" in i][0]
-        hexchat.prnt('You cannot send this message because this URL {0} is not into the filter list'.format(url))
         
+        hexchat.prnt('You cannot send this message because this URL {0} is not into the filter list'.format(url))
+        #hexchat.command('SAY {}'.format("ok man, this is working!"))
         return hexchat.EAT_ALL
 
 hexchat.hook_command("", replace_url)
