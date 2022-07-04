44c85e87768ac4328602975670c157b442264037
killown <systemofdown@gmail.com>
2022-07-04 19:04:39 -0300

    Create filter-messages.py

---
+++ filter-messages.py
@@ -0,0 +1,19 @@
+__module_name__ = "filter"
+__module_version__ = "1.0"
+__module_description__ = "Filter messages"
+
+import hexchat
+
+filters = ["https://bit.ly", "https://bpa.st/"]
+
+# Called when you send a message
+def replace_url(word, word_eol, userdata):
+	# Skip replacement if string does not match
+    if not any(filter for filter in filters if filter in word_eol[0]) and "://" in word_eol[0]:
+        url = [i for i in word_eol[0].split() if "://" in i][0]
+        
+        hexchat.prnt('You cannot send this message because this URL {0} is not into the filter list'.format(url))
+        #hexchat.command('SAY {}'.format("ok man, this is working!"))
+        return hexchat.EAT_ALL
+
+hexchat.hook_command("", replace_url)
