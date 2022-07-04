0a7aac321196fef8ff451be7dda4c9f86e0e1ec1
killown <systemofdown@gmail.com>
2022-07-04 19:28:23 -0300

    Update filter-messages.py

--- filter-messages.py
+++ filter-messages.py
@@ -13,6 +13,7 @@ if not os.path.exists(path):
 
 filters = [i.strip() for i in open(path, "r").readlines()]
 
+#do not send the message if it contains an url that isn't in the filter list
 def FilterMessage(word, word_eol, userdata):
     if not any(filter for filter in filters if filter in word_eol[0]) and "://" in word_eol[0]:
         url = [i for i in word_eol[0].split() if "://" in i][0]   
